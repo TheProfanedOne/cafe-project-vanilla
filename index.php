88d8f0442ae84efe83f78b1623fc8d571d7b311e
The Profaned One <89664730+TheProfanedOne@users.noreply.github.com>
2023-02-07 12:36:59 -0700

    test

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             <?php include INCL.'header.php' ?>
 
             <main>
-                <p>Something Else, Once Again</p>
+                <p>Hello, There</p>
                 <?php draw_specials($weekday) ?>
             </main>
 
