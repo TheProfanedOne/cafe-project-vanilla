404c564984de42f2fc4e43262ae2f39c21e1ef29
TheProfanedOne <mjm11102001@gmail.com>
2023-02-07 16:35:17 -0700

    Testing SPA part 3

--- includes/header.php
+++ includes/header.php
@@ -6,19 +6,19 @@
         <nav>
             <span>
                 <label for="h">Home</label>
-                <input type="submit" name="h" value="specials.php">
+                <input id="h" type="submit" name="h" value="specials.php">
             </span>
             <span>
                 <label for="b">Breakfast</label>
-                <input type="submit" name="b" value="b-menu.php">
+                <input id="b" type="submit" name="b" value="b-menu.php">
             </span>
             <span>
                 <label for="l">Lunch</label>
-                <input type="submit" name="l" value="l-menu.php">
+                <input id="l" type="submit" name="l" value="l-menu.php">
             </span>
             <span>
                 <label for="d">Dinner</label>
-                <input type="submit" name="d" value="d-menu.php">
+                <input id="d" type="submit" name="d" value="d-menu.php">
             </span>
         </nav>
     </form>
