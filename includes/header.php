0990935fb9b1ebd1159f2ee87c1f580acb1838a0
TheProfanedOne <mjm11102001@gmail.com>
2023-02-07 17:07:56 -0700

    GET stuff

--- includes/header.php
+++ includes/header.php
@@ -6,19 +6,19 @@
         <nav>
             <span>
                 <label for="h">Home</label>
-                <input id="h" type="submit" name="h" value="h">
+                <input id="h" type="submit" name="h" value="true">
             </span>
             <span>
                 <label for="b">Breakfast</label>
-                <input id="b" type="submit" name="b" value="b">
+                <input id="b" type="submit" name="b" value="true">
             </span>
             <span>
                 <label for="l">Lunch</label>
-                <input id="l" type="submit" name="l" value="l">
+                <input id="l" type="submit" name="l" value="true">
             </span>
             <span>
                 <label for="d">Dinner</label>
-                <input id="d" type="submit" name="d" value="d">
+                <input id="d" type="submit" name="d" value="true">
             </span>
         </nav>
     </form>
