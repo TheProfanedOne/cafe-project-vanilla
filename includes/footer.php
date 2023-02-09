a16451dda5e5dd30dd9d601748cb239980ef4106
TheProfanedOne <mjm11102001@gmail.com>
2023-02-09 13:40:41 -0700

    scss

--- includes/footer.php
+++ includes/footer.php
@@ -5,6 +5,6 @@ $location = 'Location: 264 Comer Hill Rd. Great Falls, MT 59404';
 $hours    = 'Hours: 6AM-8PM Sun-Sat';
 $phone    = 'Phone Number: (406) 750-8624';
 
-echo "<footer id=foot>$location $sep $hours $sep $phone</footer>";
+echo "<footer>$location $sep $hours $sep $phone</footer>";
 
 unset($sep, $location, $hours, $phone);
--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <header>
-    <div id="banner">
+    <div>
         <span>RISE AND GRIND</span>
     </div>
     <nav>
